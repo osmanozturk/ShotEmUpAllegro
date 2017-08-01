ab4e53ba3017a0e81e796992baa62590e57b1763
Osman Öztürk <osman.ozturk2016@gtu.edu.tr>
2017-08-01 23:18:53 +0300

    -Added movement with arrow keys -Intechanged normal and damage ship sprites
    moving faster after some time bug will be fixed on next commit

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 /*typedef enum  { false,true }bool;*/
 typedef enum  { DAMAGE, FAST, NORMAL }SHIPTYPE;
 typedef enum  { BLUE, GREEN, ORANGE, RED }SHIPCOLOR;
+enum KEYS{ UP,DOWN,LEFT,RIGHT,SPACE} ;
 
 
 void initShip(ship *ship,ALLEGRO_BITMAP *sheet ,SHIPTYPE type, SHIPCOLOR color);
@@ -27,7 +28,7 @@ int main(void) {
 
     /*variables*/
     bool done=false,redraw = false;
-
+    bool pressed[5] = {false,false,false,false,false};
     /*Allegro variables*/
 
     ALLEGRO_TIMER  *timer;
@@ -52,7 +53,7 @@ int main(void) {
     timer = al_create_timer(1.0 / FPS);
     eventQ = al_create_event_queue();
     sheet = al_load_bitmap("sheet.png");
-    initShip(&player, sheet, FAST, GREEN);
+    initShip(&player, sheet, FAST, RED);
 
     /*Event Sources*/
     al_register_event_source(eventQ, al_get_timer_event_source(timer));
@@ -66,19 +67,85 @@ int main(void) {
 
         if (input.type == ALLEGRO_EVENT_KEY_DOWN) {
             switch (input.keyboard.keycode) {
+            case ALLEGRO_KEY_UP:
+                pressed[UP] = true;
+                break;
+            case ALLEGRO_KEY_DOWN:
+                pressed[DOWN] = true;
+                break;
+            case ALLEGRO_KEY_LEFT:
+                pressed[LEFT] = true;
+                break;
+            case ALLEGRO_KEY_RIGHT:
+                pressed[RIGHT] = true;
+                break;
+            case ALLEGRO_KEY_SPACE:
+                pressed[SPACE] = true;
+                break;
             case ALLEGRO_KEY_ESCAPE:
                 done = true;
                 break;
             }
         }
 
-        else if (input.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-            done = true;
+        else if (input.type == ALLEGRO_EVENT_KEY_UP) {
+            switch (input.keyboard.keycode) {
+            case ALLEGRO_KEY_UP:
+                pressed[UP] = false;
+                break;
+            case ALLEGRO_KEY_DOWN:
+                pressed[DOWN] = false;
+                break;
+            case ALLEGRO_KEY_LEFT:
+                pressed[LEFT] = false;
+                break;
+            case ALLEGRO_KEY_RIGHT:
+                pressed[RIGHT] = false;
+                break;
+            case ALLEGRO_KEY_SPACE:
+                pressed[SPACE] = false;
+                break;
+            case ALLEGRO_KEY_ESCAPE:
+                done = true;
+                break;
+            }
+        }
 
-        else if (input.type = ALLEGRO_EVENT_TIMER) {
+        else if (input.type == ALLEGRO_EVENT_TIMER) {
             redraw = true;
         }
 
+        else if (input.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+            done = true;
+
+
+
+        /*Game update*/
+
+        if (pressed[UP]) {
+            player.y -= player.speed;
+            if ( (player.y - player.spr.h/2 < player.bound) )/*Cannot go too far*/
+                player.y = player.bound + player.spr.h/2;
+        }
+
+        else if (pressed[DOWN]) {
+            player.y += player.speed;
+            if ( (player.y + player.spr.h/2 > HEIGHT) )
+                player.y = HEIGHT - player.spr.h/2;
+        }
+
+
+        if (pressed[LEFT]) {
+            player.x -= player.speed;
+            if ( (player.x - player.spr.w/2 < 0) )
+                player.x = player.spr.w/2;
+        }
+
+        else if (pressed[RIGHT]) {
+            player.x += player.speed;
+            if ( (player.x + player.spr.w/2 > WIDTH) )
+                player.x = WIDTH - player.spr.w/2;
+        }
 
 
 
@@ -91,6 +158,7 @@ int main(void) {
             redraw = false;
             drawShip(&player);
             al_flip_display();
+            al_clear_to_color(al_map_rgb(0,0,0));
 
         }
 
@@ -111,7 +179,7 @@ void initShip(ship *ship, ALLEGRO_BITMAP *sheet,  SHIPTYPE type,SHIPCOLOR color)
     switch (type) {
     case NORMAL:
         ship->live = true;
-        ship->spr.w = 99;
+        ship->spr.w = 112;
         ship->spr.h = 75;
         ship->spr.image = al_create_bitmap(ship->spr.w, ship->spr.h);
         ship->x = WIDTH / 2;
@@ -123,22 +191,22 @@ void initShip(ship *ship, ALLEGRO_BITMAP *sheet,  SHIPTYPE type,SHIPCOLOR color)
         switch (color) {
         case BLUE:
             al_set_target_bitmap(ship->spr.image);
-            al_draw_bitmap_region(sheet, 211, 941, ship->spr.w, ship->spr.h, 0, 0, 0);
+            al_draw_bitmap_region(sheet, 112, 791, ship->spr.w, ship->spr.h, 0, 0, 0);
             al_set_target_backbuffer(display);
             break;
         case GREEN:
             al_set_target_bitmap(ship->spr.image);
-            al_draw_bitmap_region(sheet, 237, 377, ship->spr.w, ship->spr.h, 0, 0, 0);
+            al_draw_bitmap_region(sheet, 112, 866, ship->spr.w, ship->spr.h, 0, 0, 0);
             al_set_target_backbuffer(display);
             break;
         case ORANGE:
             al_set_target_bitmap(ship->spr.image);
-            al_draw_bitmap_region(sheet, 247, 84, ship->spr.w, ship->spr.h, 0, 0, 0);
+            al_draw_bitmap_region(sheet, 112, 716, ship->spr.w, ship->spr.h, 0, 0, 0);
             al_set_target_backbuffer(display);
             break;
         case RED:
             al_set_target_bitmap(ship->spr.image);
-            al_draw_bitmap_region(sheet, 224, 832, ship->spr.w, ship->spr.h, 0, 0, 0);
+            al_draw_bitmap_region(sheet, 0, 941, ship->spr.w, ship->spr.h, 0, 0, 0);
             al_set_target_backbuffer(display);
             break;
         default:
@@ -185,7 +253,7 @@ void initShip(ship *ship, ALLEGRO_BITMAP *sheet,  SHIPTYPE type,SHIPCOLOR color)
         break;
     case DAMAGE:
         ship->live = true;
-        ship->spr.w = 112;
+        ship->spr.w = 99;
         ship->spr.h = 75;
         ship->spr.image = al_create_bitmap(ship->spr.w, ship->spr.h);
         ship->x = WIDTH / 2;
@@ -197,22 +265,22 @@ void initShip(ship *ship, ALLEGRO_BITMAP *sheet,  SHIPTYPE type,SHIPCOLOR color)
         switch (color) {
         case BLUE:
             al_set_target_bitmap(ship->spr.image);
-            al_draw_bitmap_region(sheet, 112, 791, ship->spr.w, ship->spr.h, 0, 0, 0);
+            al_draw_bitmap_region(sheet, 211, 941, ship->spr.w, ship->spr.h, 0, 0, 0);
             al_set_target_backbuffer(display);
             break;
         case GREEN:
             al_set_target_bitmap(ship->spr.image);
-            al_draw_bitmap_region(sheet, 112, 866, ship->spr.w, ship->spr.h, 0, 0, 0);
+            al_draw_bitmap_region(sheet, 237, 377, ship->spr.w, ship->spr.h, 0, 0, 0);
             al_set_target_backbuffer(display);
             break;
         case ORANGE:
             al_set_target_bitmap(ship->spr.image);
-            al_draw_bitmap_region(sheet, 112, 716, ship->spr.w, ship->spr.h, 0, 0, 0);
+            al_draw_bitmap_region(sheet, 247, 84, ship->spr.w, ship->spr.h, 0, 0, 0);
             al_set_target_backbuffer(display);
             break;
         case RED:
             al_set_target_bitmap(ship->spr.image);
-            al_draw_bitmap_region(sheet, 0, 941, ship->spr.w, ship->spr.h, 0, 0, 0);
+            al_draw_bitmap_region(sheet, 224, 832, ship->spr.w, ship->spr.h, 0, 0, 0);
             al_set_target_backbuffer(display);
             break;
         default:
